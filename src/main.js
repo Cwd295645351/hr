05b8dee674c7da6da9960fa44d6d646c5366ee21
Cwd295645351 <295645351@qq.com>
2020-11-15 00:59:45 +0800

    引入默认样式和Element框架

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-14 00:10:00
+ * @LastEditTime: 2020-11-14 23:40:19
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \hr-manage\src\main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+
+import 'element-ui/lib/theme-chalk/index.css';
+import './../public/css/reset.css';
+
+Vue.use(ElementUI);
+
+
 
 Vue.config.productionTip = false
 
