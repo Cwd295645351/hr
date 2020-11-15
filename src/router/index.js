6ea259edfa2c2c3321781a85c3f4578f0c0a1902
Cwd295645351 <295645351@qq.com>
2020-11-16 00:14:13 +0800

    新增路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 陈伟栋
  * @Date: 2020-11-14 00:10:00
- * @LastEditTime: 2020-11-15 00:43:13
+ * @LastEditTime: 2020-11-15 22:47:57
  * @LastEditors: Please set LastEditors
  * @Description: 路由配置
  * @FilePath: \hr-manage\src\router\index.js
@@ -11,15 +11,31 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
+const routes = [{
     path: "/",
     redirect: "/login",
   },
   {
-    path: "/login",
+    path: "/login", // 登录页
     name: "Login",
     component: () => import("../views/Login/Login.vue"),
+  },
+  {
+    path: "/main", // 主页面
+    name: "Main",
+    component: () => import("../views/Main/Main.vue"),
+    children: [{
+      path: "interview", // 面试系统
+      component: () => import("../views/Interview/Interview.vue"),
+    }, {
+      path: "user",
+      name: "User",
+      component: () => import("../views/User/User.vue")
+    }, {
+      path: "salary",
+      name: "Salary",
+      component: () => import("../views/Salary/Salary.vue")
+    }]
   },
   {
     path: "/error",
