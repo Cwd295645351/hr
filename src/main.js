3059ee016bfbab5bc71746e508801da9c2b84b0f
Cwd295645351 <295645351@qq.com>
2020-11-30 23:49:54 +0800

    添加moment

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Chenweidong
  * @Date: 2020-11-14 00:10:00
- * @LastEditTime: 2020-11-29 14:35:02
+ * @LastEditTime: 2020-11-30 22:11:22
  * @LastEditors: Chen Weidong
  * @Description: 基础配置
  * @FilePath: \hr-manage\src\main.js
@@ -12,12 +12,14 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import echarts from 'echarts';
+import moment from 'moment';
 
 import 'element-ui/lib/theme-chalk/index.css';
 import './../public/css/reset.less';
 
 Vue.use(ElementUI);
 Vue.prototype.$echarts = echarts
+Vue.prototype.$moment = moment;
 
 
 Vue.config.productionTip = false
