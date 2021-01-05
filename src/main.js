ad24d019ca41f7966b74b71dd5c2d70f76779659
CYD <295645351@qq.com>
2021-01-05 22:30:28 +0800

    添加路由导航守卫

--- src/main.js
+++ src/main.js
@@ -4,28 +4,44 @@
  * @Author: Chen
  * @Date: 2020-12-17 22:42:22
  * @LastEditors: Chen
- * @LastEditTime: 2020-12-17 23:02:31
+ * @LastEditTime: 2021-01-05 22:11:58
  */
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import ElementUI from 'element-ui';
-import echarts from 'echarts';
-import moment from 'moment';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementUI from "element-ui";
+import echarts from "echarts";
+import moment from "moment";
 
-import 'element-ui/lib/theme-chalk/index.css';
-import './../public/css/reset.less';
+import "element-ui/lib/theme-chalk/index.css";
+import "./../public/css/reset.less";
 
 Vue.use(ElementUI);
-Vue.prototype.$echarts = echarts
+Vue.prototype.$echarts = echarts;
 Vue.prototype.$moment = moment;
 
+router.beforeEach((to, from, next) => {
+  console.log(to.path);
+  debugger
+	if (to.path == "/" || to.path == "/login" || to.path == "/error") {
+		next();
+	} else {
+		const token = sessionStorage.getItem("token");
+		if (token) {
+			next();
+		} else {
+			next({
+				path: "/login"
+			});
+		}
+	}
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: (h) => h(App)
+}).$mount("#app");
