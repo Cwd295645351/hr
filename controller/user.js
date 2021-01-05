3e8c4420b47eb403d02b99c6bbfec5943ba81f15
CYD <295645351@qq.com>
2021-01-05 23:57:37 +0800

    完善登录接口

--- controller/user.js
+++ controller/user.js
@@ -1,21 +1,22 @@
 /*
- * @Description: 登录接口
+ * @Description: 登录接口相关数据库操作
  * @Version: 0.0.1
  * @Author: Chen
  * @Date: 2021-01-03 22:16:09
  * @LastEditors: Chen
- * @LastEditTime: 2021-01-04 23:57:33
+ * @LastEditTime: 2021-01-05 23:57:20
  */
 
 // 预防xss攻击
-// import xss from "xss";
+import xss from "xss";
 
 import genPassword from "../utils/cryp";
 import User from "../db/models/User";
 import JWT from "../utils/jwt";
 
 const login = async (username, password) => {
-	password = genPassword(password); // 对密码进行加密
+	username = xss(username);
+	password = xss(genPassword(password)); // 对密码进行加密
 	console.log(password);
 	// JWT();
 	const res = await User.findOne({
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 /*
- * @Description:
- * @Version:
- * @Author:
+ * @Description: 用户登录路由
+ * @Version: 0.0.1
+ * @Author: Chen
  * @Date: 2020-12-29 00:00:00
  * @LastEditors: Chen
- * @LastEditTime: 2021-01-03 23:13:28
+ * @LastEditTime: 2021-01-05 23:56:57
  */
 import Router from "koa-router";
 import login from "../controller/user";
@@ -21,9 +21,12 @@ router.post("/login", async (ctx, next) => {
 	const { username, password } = ctx.request.body;
 	const res = await login(username, password);
 	if (res) {
+		// 数据库存在用户
+		// console.log("1111",res);
+		ctx.session.token = res.token;
 		ctx.body = new SuccessModel(res, "登录成功");
 	} else {
-		ctx.body = new ErrorModel("登录失败");
+		ctx.body = new ErrorModel(null, "登录失败");
 	}
 });
 
