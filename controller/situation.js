d032430e75e4194360a63633b0c4605d8e7d6916
CYD <295645351@qq.com>
2021-01-10 17:39:01 +0800

    完善面试情况接口

--- controller/situation.js
+++ controller/situation.js
@@ -4,33 +4,40 @@
  * @Author: Chen
  * @Date: 2021-01-05 22:39:09
  * @LastEditors: Chen
- * @LastEditTime: 2021-01-05 23:47:57
+ * @LastEditTime: 2021-01-10 17:36:23
  */
 
 import xss from "xss";
 
 import Interviewees from "../db/models/Interviewees";
+import { getMajorNameById, getChannelNameById } from "./common";
 
+// 遍历对象，将对象属性进行xss防御
 const xssData = (data) => {
 	if (typeof data == "string" || typeof data == "number") {
 		return xss(data);
 	} else {
 		for (let key in data) {
 			data[key] = xssData(data[key]);
-        }
-        return data;
+		}
+		return data;
 	}
 };
 
+// 获取面试情况列表
 export const getList = async () => {
 	const res = await Interviewees.find();
 	return res;
 };
 
+// 添加面试者
 export const addInterviewee = async (data) => {
 	xssData(data);
 	console.log("test:", data);
+	const majorName = await getMajorNameById(data.majorId);
+	data.majorName = majorName;
+	const channelName = await getChannelNameById(data.channelId);
+	data.channelName = channelName;
 	const res = await Interviewees.create(data);
-	// console.log("res", res);
 	return res;
 };
