140a8765c9d624082b431cb40a20f439cd2927f7
CYD <295645351@qq.com>
2021-01-25 22:43:09 +0800

    公共方法修改

--- utils/tools.js
+++ utils/tools.js
@@ -4,24 +4,58 @@
  * @Author: Chen
  * @Date: 2021-01-23 13:21:47
  * @LastEditors: Chen
- * @LastEditTime: 2021-01-24 22:32:35
+ * @LastEditTime: 2021-01-25 21:40:56
  */
 
-const tools = {
-	// 获取用户信息
-	getInfo: () => {
-		const userInfo = sessionStorage.getItem("userInfo")
-			? JSON.parse(sessionStorage.getItem("userInfo"))
-			: null;
-		return userInfo || null;
-	},
-	// 获取心跳时间
-	getExpire: () => {
-		const expire = sessionStorage.getItem("expireAt")
-			? JSON.parse(sessionStorage.getItem("expireAt"))
-			: 0;
-		return expire || 0;
-	}
-};
+const USER_INFO_KEY = "userInfo";
+const EXPIRE_AT = "expiresAt";
 
-export default tools;
+// 获取完整登录信息
+export function getUserInfo(){
+    const userInfoStr = window.sessionStorage.getItem(USER_INFO_KEY);
+    const userInfo = userInfoStr && JSON.parse(userInfoStr);
+    return userInfo || null;
+}
+
+// 退出登录
+export function logout() {
+    window.sessionStorage.removeItem(USER_INFO_KEY);
+    window.sessionStorage.removeItem(EXPIRE_AT);
+    
+    if( window.refreshTokenTimer ){
+        clearTimeout(window.refreshTokenTimer);
+        window.refreshTokenTimer = null;
+    }
+    window.location.reload();    
+
+}
+
+// 获取token过期时长
+export function getExpireSeconds() {
+    const userInfo = getUserInfo();
+    const expireSeconds = userInfo && userInfo.expiresIn;
+    return expireSeconds;
+}
+
+// 刷新需要主动刷新token时间戳
+export function refreshExpireStamp( secondsExpiresIn ) {
+    if( secondsExpiresIn > 0 ){
+        const MS_ONE_SECOND = 1000;
+        const nextStamp = new Date().getTime() + secondsExpiresIn * MS_ONE_SECOND;
+        sessionStorage.setItem(
+            EXPIRE_AT,
+            nextStamp,
+        );
+    }
+}
+
+// 刷新用户信息
+export function refreshUserInfo(refreshInfo){
+    let userInfo = getUserInfo();
+    userInfo = {
+        ...userInfo,
+        ...refreshInfo,
+    };
+    window.sessionStorage.setItem(USER_INFO_KEY, JSON.stringify(userInfo));
+}
+
