2d642edabc0d7109ebbb88cbcb6b66731be56724
CYD <295645351@qq.com>
2021-01-25 22:42:57 +0800

    路由配置修改

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,18 @@
  * @Author: Chen
  * @Date: 2020-12-17 22:42:22
  * @LastEditors: Chen
- * @LastEditTime: 2021-01-24 22:36:35
+ * @LastEditTime: 2021-01-25 22:42:21
  */
 import Vue from "vue";
 import VueRouter from "vue-router";
+import * as tools from "../../utils/tools";
 
 Vue.use(VueRouter);
 
 const routes = [
 	{
 		path: "/",
+		name: "Login",
 		redirect: "/login"
 	},
 	{
@@ -29,6 +31,7 @@ const routes = [
 		children: [
 			{
 				path: "interview", // 面试系统
+				name: "Interview",
 				component: () => import("../views/Interview/Interview.vue")
 			},
 			{
@@ -50,6 +53,7 @@ const routes = [
 	},
 	{
 		path: "*",
+		name: "*",
 		redirect: "/error"
 	}
 ];
@@ -58,18 +62,20 @@ const router = new VueRouter({
 	routes
 });
 
+// 不需要重定向页面name
+const NOT_NEED_TOKEN_ROUTES = ["Login"];
+
 router.beforeEach((to, from, next) => {
-	if (to.path == "/" || to.path == "/login" || to.path == "/error") {
+	if (NOT_NEED_TOKEN_ROUTES.indexOf(to.name) === -1 && !tools.getUserInfo()) {
+		next({ path: "/login" });
+	} else if (
+		NOT_NEED_TOKEN_ROUTES.indexOf(to.name) !== -1 &&
+		tools.getUserInfo() !== null
+	) {
+		sessionStorage.clear();
 		next();
 	} else {
-		const userInfo = JSON.parse(sessionStorage.getItem("userInfo"));
-		if (userInfo) {
-			next();
-		} else {
-			next({
-				path: "/login"
-			});
-		}
+		next();
 	}
 });
 
